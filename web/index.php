e332cd3769e52ea605c893745045a3a91100a53e
Gooicoo <didaclopez93@gmail.com>
2020-02-21 20:08:31 +0100

    gooicoo web

--- web/index.php
+++ web/index.php
@@ -8,7 +8,14 @@
 
     <h1>Gooicoo web</h1>
 
+    <?php
+    $dbopts = parse_url(getenv('DATABASE_URL'));
+    // Conectando y seleccionado la base de datos
+    $dbconn = pg_connect("host=localhost dbname=publishing user=www password=foo")
+        or die('No se ha podido conectar: ' . pg_last_error());
 
+    echo 'CONNECTAT!';
+    ?>
 
   </body>
 </html>
