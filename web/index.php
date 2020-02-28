1caaf18cf1c9b68cb381c23f8e201db6ef854361
Gooicoo <didaclopez93@gmail.com>
2020-02-28 19:05:51 +0100

    conn

--- web/index.php
+++ web/index.php
@@ -8,6 +8,13 @@
 
     <h1>Gooicoo web</h1>
 
+    <form class="" action="index.php" method="post">
+      <input type="text" name="" value="">User
+      <input type="password" name="" value="">Pass
+      <input type="submit" name="" value="ENTRAR">
+    </form>
+
+
     <?php
     $dbopts = getenv('DATABASE_URL');
     // Conectando y seleccionado la base de datos
